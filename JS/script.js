f77a43f34e1d1cab2a24dbd19513a63b23d574e4
Leonardo Augusto <40269047+LeoAuguszto@users.noreply.github.com>
2024-05-26 16:24:20 -0300

    Portifolio upoad
    First version

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+const MenuHamburguer = document.querySelector('.menu-hamburguer')
+
+MenuHamburguer.addEventListener('click',()=>{
+    toggleMenu();
+});
+
+function toggleMenu (){
+    const nav = document.querySelector('.nav');
+    MenuHamburguer.classList.toggle('change');
+
+    if(MenuHamburguer.classList.contains('change')){
+        nav.style.display ='block';
+    }else{
+        nav.style.display ='none';
+    }
+}
+
+function ChangeSize(){
+    const nav = document.querySelector('.nav');
+    if(window.innerWidth >= 768){
+        nav.style.display ='block'
+        console.log('passou')
+    }else{
+        nav.style.display ='none';
+    }
+}
